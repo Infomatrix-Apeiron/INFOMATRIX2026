d5fa63b81fde8eb765ea2a72d609f569e32c0ffb
v.turenko <vitalii.turenko@dah-online.com>
2026-05-15 16:57:07 +0300

    Init commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# INFOMATRIX2026
+# FrontentTemplate
 
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.2.11.
+This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.1.4.
 
 ## Development server
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {App} from './app';
+import {Dev} from './shared/ui/dev/dev';
+import {Onboarding} from './features/onboarding/onboarding';
+import {CraftFlowState} from './features/craft/craft-flow.state';
+import {DescribeStep} from './features/craft/describe-step/describe-step';
+import {StartStep} from './features/craft/start-step/start-step';
+import {IdeasListStep} from './features/craft/ideas-list-step/ideas-list-step';
+import {IdeaDetailStep} from './features/craft/idea-detail-step/idea-detail-step';
+import {ResultStep} from './features/craft/result-step/result-step';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: App,
+        children: [
+            {
+                path: 'craft',
+                providers: [CraftFlowState],
+                children: [
+                    {path: 'start', component: StartStep},
+                    {path: 'describe', component: DescribeStep},
+                    {path: 'generating-ideas', component: IdeasListStep},
+                    {path: 'generating-idea', component: IdeaDetailStep},
+                    {path: 'result', component: ResultStep},
+                    {path: '**', redirectTo: 'start'},
+                ]
+            },
+            {path: 'onboarding', component: Onboarding},
+            {path: 'dev', component: Dev},
+            {path: '**', redirectTo: 'craft'},
+        ]
+    }
+];
