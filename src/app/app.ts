87abae86ed10063ab5dec20eef7fa905d355eebf
v.turenko <vitalii.turenko@dah-online.com>
2026-05-16 02:37:02 +0300

    Added user identifier

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,4 +8,17 @@ import {RouterOutlet} from '@angular/router';
     styleUrl: './app.scss'
 })
 export class App {
+    userId = '';
+
+    ngOnInit(): void {
+        const key = 'userId';
+        let id = localStorage.getItem(key);
+
+        if (!id) {
+            id = crypto.randomUUID();
+            localStorage.setItem(key, id);
+        }
+
+        this.userId = id;
+    }
 }
